61d744fc547ab6aeb4c7ff844ffc34d186e8aaf7
Uladzimir Luhautsou <lugovczov@gmail.com>
2021-12-22 20:00:59 +0300

    refactor: mui theme for button to constant

--- src/components/range-tab-item/RangeTabItem.jsx
+++ src/components/range-tab-item/RangeTabItem.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import {TAB_NAMES} from "../../constants/date-names";
-import {Button, createTheme} from "@mui/material";
+import {Button} from "@mui/material";
 import {ThemeProvider} from "@emotion/react";
 import './range-tab-item.css'
+import {neutralButtonTheme} from "../../constants/mui-themes";
 
 export const RangeTabItem = ({ id, handleClick, active }) => {
-  const theme = createTheme({
-    palette: {
-      neutral: {
-        main: '#000000DE',
-        contrastText: '#fff',
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={neutralButtonTheme}>
       <Button
         onClick={() => handleClick()}
         className={`rounded text-sm px-3 h-11 w-32 ${active ? 'range-tab-item--active' : ''}`}
---
+++ src/constants/mui-themes.js
@@ -0,0 +1,10 @@
+import {createTheme} from "@mui/material";
+
+export const neutralButtonTheme = createTheme({
+  palette: {
+    neutral: {
+      main: '#000000DE',
+      contrastText: '#fff',
+    },
+  },
+});
